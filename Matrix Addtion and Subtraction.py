fb9febec88853ef9696d5e6dc1905e923de61de6
sy-zaid <syedmzaid.22@gmail.com>
2022-09-11 08:01:31 +0500

    Update Matrix Addtion and Subtraction.py

--- Matrix Addtion and Subtraction.py
+++ Matrix Addtion and Subtraction.py
@@ -6,7 +6,6 @@ def mat_add(mat1,mat2):
         res[i]+=mat1[i]+mat2[i]
     return res
 
-
 def mat_sub(mat1,mat2):
     res=[0,0,0,0]
     for i in range(4):
@@ -14,10 +13,8 @@ def mat_sub(mat1,mat2):
     return res
 
 
-
 mat1=[5,2,3,4]
 mat2=[1,2,3,4]
 
 print(mat_add(mat1,mat2))
-
 print(mat_sub(mat1,mat2))
